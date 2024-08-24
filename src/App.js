5063ccf984c2658a52a851915c882c7715d7545e
Ashish Raghav <ashishraghav2468@gmail.com>
2024-08-24 18:12:08 +0530

    Header prototype - ashish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div
-      className=''
-    >
-      this is new app .sj
+    <div>
+      <Header/>
     </div>
   )
 }
